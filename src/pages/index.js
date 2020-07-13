25cce6a040b301ec28b63d6980f9eee560efc841
Adriaan Knapen <hi@aknapen.nl>
2020-07-13 20:21:44 +0200

    feat(seo): sets the title and description of this website

--- src/pages/index.js
+++ src/pages/index.js
@@ -59,8 +59,8 @@ function Home() {
   const {siteConfig = {}} = context;
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={``} // Set this to prefix the title
+      description="Documentation of ClaimR, providing verified locations of your user by analysing raw GPS measurements data.">
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
