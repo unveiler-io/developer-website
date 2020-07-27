e3cc8b961abfc019a04397f1c52d3bbff106e315
Adriaan Knapen <hi@aknapen.nl>
2020-07-27 15:03:05 +0200

    feat(sidebar): prevent collapsing categories by default

--- sidebars.js
+++ sidebars.js
@@ -1,12 +1,27 @@
 module.exports = {
-  docs: {
-    ClaimR: ['overview', 'use-cases'],
-    'ClaimR API': ['api/getting-started', 'api/jwts', 'api/schema'],
-    'React Native Client': [
-      'react-native/overview',
-      'react-native/requirements',
-      'react-native/getting-started',
-      'react-native/api',
-    ],
-  },
+  docs: [
+    {
+      type: 'category',
+      label: 'ClaimR',
+      items: ['overview', 'use-cases'],
+      collapsed: false,
+    },
+    {
+      type: 'category',
+      label: 'ClaimR API',
+      items: ['api/getting-started', 'api/jwts', 'api/schema'],
+      collapsed: false,
+    },
+    {
+      type: 'category',
+      label: 'React Native Client',
+      collapsed: false,
+      items: [
+        'react-native/overview',
+        'react-native/requirements',
+        'react-native/getting-started',
+        'react-native/api',
+      ],
+    },
+  ],
 }
