6b1894e7ba9aa2eff85b1992b52b5a1f728dcff9
Adriaan Knapen <hi@aknapen.nl>
2020-09-27 09:48:49 +0200

    feat(header): adds pricing to the header

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,6 +24,7 @@ module.exports = {
           label: 'Docs',
           position: 'left',
         },
+        { to: 'docs/pricing', label: 'Pricing', position: 'left' },
         { to: 'blog', label: 'Blog', position: 'left' },
         {
           href: 'https://github.com/ClaimR',
